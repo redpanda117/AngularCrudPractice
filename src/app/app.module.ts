445336c6b3504676ecf64a1bc17a87636363843f
Tina Nguyen <nguyentina2005@yahoo.com>
2018-03-22 11:03:40 -0400

    product route work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,8 @@ import { ProductComponent } from './product/product.component';
     BrowserModule,
     HttpModule,   
     RouterModule.forRoot([
-      {path: "", component: HomeComponent}
+      {path: "", component: HomeComponent},
+      {path: "product", component: ProductComponent}
     ])
     ],
   providers: [],
